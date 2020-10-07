a82ac88222b5d3fc117e1171497bc31cc95826c0
Stradmann <57647277+Stradmann@users.noreply.github.com>
2020-10-07 19:50:29 +0200

    saving changes

--- Practica06/src/Bank/Accounts.java
+++ Practica06/src/Bank/Accounts.java
@@ -10,107 +10,89 @@ public class Accounts {
 		ArrayList<Account> accountsArray = new ArrayList<>();
 		
 		while (true) {
-			System.out.println("Name:");
-			String name = read.nextLine();
+			boolean exit = false;
+			System.out.println("Do you want to do any operation?");
+			System.out.printlm("Open new (A)ccount, (D)eposit, (W)ithdraw, (T)ransfer, Get (C)redit, e(X)it");
+			String operation = read.nextLine();
 			
-			if (name.equals("")) {
+			switch (operation) {
+			
+			case "A":
+				
+				System.out.println("What's your name?");
+				String name = read.nextLine();
+				System.out.println("How many money do you have?")
+				double initialDeposit = Double.valueOf(read.nextLine());
+				
+				Account newAccount = new Account(name, initialDeposit);
+				accountsArray.add(newAccount);
+				System.out.println("Account seccesfully created \n" + newAccount);
+				
+				extit = false;
+				
 				break;
+				
+			case "D":
+				
+				System.out.println("What's your name?");
+				String owner = read.nextLine();
+				int accountIndex = getAccount(accountsArray, owner);
+				if (accountIndex == -1) {
+					System.out.println("You don't have any account");
+					exit = false;
+					break;
+				}
+				Account account = accountsArray.get(accountIndex);
+				System.out.println("How much you want to deposit?");
+				double deposit = Double.valueOf(read.nextLine());
+				account.deposit(deposit);
+				System.out.println(account);
+				
+				exit = false;
+				
+				break;
+				
+			case "W":
+				
+				System.out.println("What's your name?");
+				String owner = read.nextLine();
+				int accountIndex = getAccount(accountsArray, owner);
+				if (accountIndex == -1) {
+					System.out.println("You don't have any account");
+					exit = false;
+					break;
+				}
+				Account account = accountsArray.get(accountIndex);
+				System.out.println("How much you want to withdraw?");
+				double withdraw = Double.valueOf(read.nextLine());
+				
+				break;
+			case "T":
+				break;
+			case "C":
+				break;
+			case "X":
+				break;
+			default:
+				break;	
 			}
-			
-			System.out.println("How many money?");
-			double manyMoney = Double.valueOf(read.nextLine());
-			
-			accountsArray.add(new Account(name, manyMoney));
 		}
 		
-		transfer(accountsArray);
-		
 		read.close();
+	}
+	
+	public static int getAccount (ArrayList<Account> accounts, String owner) {
+		
+		for (int i = 0; i < accounts.size(); i++) {
+			if (accounts.get(i).getOwner.equals(owner)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
-	public static void transfer (ArrayList<Account> accounts) {
-		Scanner read = new Scanner(System.in);
+	public static boolean haveFounds (Account account, double value) {
 		
-		System.out.println("Who are you?");
-		String owner = read.nextLine();
-		boolean ownerExistsLikeTeruel = false;
-		int ownerAccountIndex = 0;
-		for(int i = 0; i < accounts.size(); i++) {
-			if (accounts.get(i).getOwner().equals(owner)) {
-				ownerExistsLikeTeruel = true;
-				ownerAccountIndex = i;
-				break;
-			}
-		}
-		String reciever = "";
-		double manyMoney = 0.0;
-		if (ownerExistsLikeTeruel) {
-			System.out.println("How many money want to transefer?");
-			manyMoney = Double.valueOf(read.nextLine());
-			System.out.println("To who? (Don't worry, Doctor will not recive nothing)");
-			reciever = read.nextLine();
-		} else {
-			read.close();
-			return;
-		}
-		boolean recieverExistsToo = false;
-		int recieverAccounIndex = 0;
-		for(int i = 0; i < accounts.size(); i++) {
-			if (accounts.get(i).getOwner().equals(reciever)) {
-				recieverExistsToo = true;
-				recieverAccounIndex = i;
-				break;
-			}
-		}
-		
-		if (recieverExistsToo) {
-			accounts.get(ownerAccountIndex).withdrawal(manyMoney);
-			accounts.get(recieverAccounIndex).deposit(manyMoney);
-		}
-		
-		read.close();
+		if (account.getBalance() >= value)
 	}
-	
-	public static void personalCredit  (ArrayList<Account> accounts) {			
-		Scanner read = new Scanner(System.in);
-		
-		System.out.println("Who are you?");
-		String owner = read.nextLine();
-		boolean ownerExistsLikeTeruel = false;
-		int ownerAccountIndex = 0;
-		
-		for(int i = 0; i < accounts.size(); i++) {
-			if (accounts.get(i).getOwner().equals(owner)) {
-				ownerExistsLikeTeruel = true;
-				ownerAccountIndex = i;
-				break;
-			}
-		}
-			
-		System.out.println("How much you want?");
-		double needed = Double.valueOf(read.nextLine());
-		System.out.println("Do you really need it? (Y)es, (N)o");
-		String answer = read.nextLine();
-		
-		if (answer.equals("Y")) {
-			System.out.println("Sorry, we don't work with homless. But we can offer that credit with 250% of interest rate.");
-			read.close();
-			return;
-		} else if (answer.equals("N")) {
-			System.out.println("That's just right. Let's check it");
-			if(ownerExistsLikeTeruel && accounts.get(ownerAccountIndex).getBalance() > needed) {
-				accounts.get(ownerAccountIndex).deposit(needed);
-			}else{
-				System.out.println("Get out!");
-			}
-		}
-		
-		read.close();
-	}
-
-
-	public static void yourMethod () {
-		
-	}
-
 }
