bf73762dcbee2a0b7e4ac7c49ede6f2658a8d420
Stradmann <57647277+Stradmann@users.noreply.github.com>
2020-10-20 06:23:24 +0200

    something added

---
+++ Practica12/src/controller/Main.java
@@ -0,0 +1,11 @@
+package controller;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner read = new Scanner(System.in);
+	}
+}
---
+++ Practica12/src/menu/MainMenu.java
@@ -0,0 +1,12 @@
+package menu;
+
+import java.util.Scanner;
+
+public class MainMenu {
+	
+	public static void mainMenu(Scanner read) {
+		
+		System.out.println("Welcome! Choose your option:\n (R)egister\n (L)ogin");
+		
+	}
+}
